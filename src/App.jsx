482fd7f71ea53d910abe6a2e30b16236b66cb9be
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2023-12-11 11:51:36 +0100

    add states of CV data to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
+import exampleData from "./example-data";
 
-function App() {}
+function App() {
+  const [personalInfo, setPersonalInfo] = useState(exampleData.personalInfo);
+  const [sections, setSections] = useState(exampleData.sections);
+}
 
 export default App;
